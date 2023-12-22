de4034abb86dba73fff891661c71de3f7d1fdbd3
Matrix30 <Matrixboy30@gmail.com>
2023-12-22 19:19:20 +0100

    fix"

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
-# install flask
-package { 'python3-pip':
-  ensure => present,  # Ensure pip3 is installed
-}
-
-package { 'flask':
-  ensure   => '2.1.0',  # Specify the exact version
-  provider => 'pip3',  # Use pip3 for installation
-  require  => Package['python3-pip'],  # Depend on pip3 being installed first
+# Install the puppet-lint package
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem',
 }
