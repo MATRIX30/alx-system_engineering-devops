5c23ea2807d86a3ceeaa1071988f804c83cafb13
Matrix30 <Matrixboy30@gmail.com>
2023-12-25 08:18:22 +0100

    finish task 0 create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,16 @@
-# Creates a file
-file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+# Using Puppet, create a file in /tmp.
+# Requirements:
+#	File path is /tmp/school
+#	File permission is 0744
+#	File owner is www-data
+#	File group is www-data
+#	File contains I love Puppet
+
+file { 'file_in_tmp':
+    ensure  => file,
+    path    => '/tmp/school',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+    mode    => '0744'
 }
