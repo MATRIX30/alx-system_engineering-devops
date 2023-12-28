14faed6b0f5e35f27e45c258fc4da6edd9d23ea2
Matrix30 <Matrixboy30@gmail.com>
2023-12-28 19:32:16 +0100

    finish install package flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,25 +4,26 @@
 #   Version must be 2.1.0
 
 # ensure python is installed
-package {'python':
+package {'python3.8':
   ensure => '3.8.10',
 }
 
 # ensure pip3 is installed
 package {'python3-pip':
-  ensure => present,
+  ensure  => present,
+  # require => Package['python3.8'],
 }
 
 # install flask
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python', 'python3-pip'],
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
 
 # install Werkzeug 2.1.1
 package {'Werkzeug':
   ensure   => '2.1.1',
-  provider => 'pip3',
-  require  => Package['python', 'python3-pip'],
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
