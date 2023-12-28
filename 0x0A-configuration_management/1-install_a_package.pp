07439453513064e686d3ebcc164872cf15fbe9cb
Matrix30 <Matrixboy30@gmail.com>
2023-12-28 18:07:33 +0100

    finish task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,25 +1,43 @@
-# Installs flask and required packages
+# Using Puppet, install flask from pip3.
 
-# Install Python 3.8.10
-package { 'python3.8':
-  ensure => '3.8.10',
-}
+#  Requirements:
 
-# Install pip
-package { 'python3-pip':
-  ensure => present,
-}
+#   Install flask
+#   Version must be 2.1.0
+# package {'python3.8':
 
-# Install Flask 2.1.0
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
-  require  => Package['python3-pip'],
-}
+# ensure => '3.8.10',
 
-# Install Werkzeug 2.1.1
-package { 'werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip',
-  require  => Package['python3-pip'],
+# }
+
+# 
+
+# package {'python3-pip':
+
+# ensure => installed,
+
+# require => Package['python3.8'],
+
+# }
+
+# package { 'Werkzeug':
+
+# ensure => '2.1.1',
+
+# require => Package['python3.8'],
+
+# }
+# package {'flask':
+
+# ensure => '2.1.0',
+
+# provider => 'pip3',
+
+# # require => Package['python3.8','python3-pip', 'Werkzeug'],
+
+# }
+
+exec {'flask-install':
+  command => 'pip3 install flask==2.1.0',
+  path    => '/usr/bin'
 }
