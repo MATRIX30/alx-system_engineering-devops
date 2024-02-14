6a63e8a834fdd55e677c7eddc82955358c6f4f86
Matrix30 <Matrixboy30@gmail.com>
2024-02-14 03:33:38 +0100

    finish

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,35 +1,24 @@
 #!/usr/bin/python3
 """
-this is a module to the number of subscribers to a subreddit
+queries the Reddit API and returns the number
+of subscribers (not active users, total subscribers)
+for a given subreddit. If an invalid subreddit is given,
+the function should return 0.
 """
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Write a function that queries the Reddit API and returns
-    the number of subscribers (not active users, total subscribers)
-    for a given subreddit. If an invalid subreddit is given, the
-    function should return 0.
+    """returns number of total subscribers"""
+    url = ("https://api.reddit.com/r/{}/about".format(subreddit))
+    headers = {'User-Agent': 'CustomClient/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    Hint: No authentication is necessary for most features of the Reddit
-    API. If you’re getting errors related to Too Many Requests, ensure you’re
-    setting a custom User-Agent.
+    if response.status_code != 200:
+        return (0)
+    response = response.json()
+    if 'data' in response:
+        return (response.get('data').get('subscribers'))
 
-    Requirements:
-
-    Prototype: def number_of_subscribers(subreddit)
-    If not a valid subreddit, return 0.
-    NOTE: Invalid subreddits may return a redirect to search results. Ensure
-    that you are not following redirects.
-    """
-    import requests
-
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    header = {
-        'User-Agent': 'alx-client/1.0'
-    }
-    data = requests.get(url, headers=header, allow_redirects=False)
-    status_code = data.status_code
-    if (status_code == 200):
-        return data.json()["data"]["subscribers"]
-    return 0
+    else:
+        return (0)
