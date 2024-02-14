2edfcbecd0bd9ef4e613eff6aebdf01d510d8c8b
Matrix30 <Matrixboy30@gmail.com>
2024-02-14 03:36:18 +0100

    lets see

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
 """
-this is a module to return top 10 hot topics for a subreddit
+queries the Reddit API and prints the titles of
+the first 10 hot posts listed for a given subreddit.
 """
+import requests
 
 
 def top_ten(subreddit):
     """
-    Write a function that queries the Reddit API and prints
-    the titles of the first 10 hot posts listed for a given subreddit.
-
-    Requirements:
-    Prototype: def top_ten(subreddit)
-    If not a valid subreddit, print None.
-    NOTE: Invalid subreddits may return a redirect to search results.
-    Ensure that you are not following redirects.
+    prints the titles of the first 10 hot posts listed for
+    a given subreddit
     """
-    import requests
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    header = {
-        'User-Agent': 'alx-client/1.0'
-    }
-    data = requests.get(url, allow_redirects=False, headers=header)
-    if (data.status_code == 200):
-        hot_topics = data.json()["data"]["children"]
-        for item in hot_topics:
-            print(item["data"]["title"])
+    url = ("https://api.reddit.com/r/{}?sort=hot&limit=10".format(subreddit))
+    headers = {'User-Agent': 'CustomClient/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        print(None)
+        return
+    response = response.json()
+    if 'data' in response:
+        for posts in response.get('data').get('children'):
+            print(posts.get('data').get('title'))
+
     else:
         print(None)
