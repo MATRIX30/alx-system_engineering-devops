b7bf87ce27ddc1ac48fc077dd1835eeaf597d120
Matrix30 <Matrixboy30@gmail.com>
2024-02-14 03:42:16 +0100

    finish those x

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,41 @@
 #!/usr/bin/python3
-"""
-Querries the Reddit API and prints the titles of the first 10 hot posts listed
-for a given subreddit
-"""
-
-import json
+'''A module containing functions for working with the Reddit API.
+'''
 import requests
 
 
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
+
+
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts listed for a subreddit"""
-    headers = {"user-agent": "holberton"}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    subdata = requests.get(url, headers=headers)
-    if subdata.status_code != 200:
-        print("None")
-        return
-    data = json.loads(subdata.text).get('data').get('children')
-    if not data:
-        print("None")
-        return
-    for item in data[0:10]:
-        print(item.get('data').get('title'))
+    '''Retrieves the title of the top ten posts from a given subreddit.
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    sort = 'top'
+    limit = 10
+    res = requests.get(
+        '{}/r/{}/.json?sort={}&limit={}'.format(
+            BASE_URL,
+            subreddit,
+            sort,
+            limit
+        ),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        for post in res.json()['data']['children'][0:10]:
+            print(post['data']['title'])
+    else:
+        print(None)
